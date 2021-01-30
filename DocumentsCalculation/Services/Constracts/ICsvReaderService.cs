8b78cf217e72872ccf8e3f09527b88350aa7f13f
Georgi Stankov <vangog06406@gmail.com>
2021-01-30 18:25:36 +0200

    Read csv file data

---
+++ DocumentsCalculation/Services/Constracts/ICsvReaderService.cs
@@ -0,0 +1,12 @@
+﻿using DocumentsCalculation.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DocumentsCalculation.Services.Constracts
+{
+    public interface ICsvReaderService
+    {
+        Task<ICollection<CustomerDataModel>> ReadCsvAsync(IFormFile csvFile);
+    }
+}
---
+++ DocumentsCalculation/Services/Implementations/CsvReaderService.cs
@@ -0,0 +1,47 @@
+﻿using DocumentsCalculation.Models;
+using DocumentsCalculation.Models.Enums;
+using DocumentsCalculation.Services.Constracts;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DocumentsCalculation.Services.Implementations
+{
+    public class CsvReaderService : ICsvReaderService
+    {
+        public async Task<ICollection<CustomerDataModel>> ReadCsvAsync(IFormFile csvFile)
+        {
+            ICollection<CustomerDataModel> customerData = new List<CustomerDataModel>();
+
+            using (StreamReader reader = new StreamReader(csvFile.OpenReadStream()))
+            {
+                //Skip headers
+                await reader.ReadLineAsync();
+
+                while (!reader.EndOfStream)
+                {
+                    string line = await reader.ReadLineAsync();
+                    string[] values = line.Split(',');
+
+                    bool isValidType = Enum.TryParse(values[3], true, out InvoiceType type);
+                    bool isValidTotal = decimal.TryParse(values[6], out decimal total);
+
+                    customerData.Add(new CustomerDataModel
+                    {
+                        Customer = values[0],
+                        VatNumber = values[1],
+                        DocumentNumber = values[2],
+                        Type = isValidType ? type : InvoiceType.Invalid,
+                        ParentDocument = values[4],
+                        Currency = values[5],
+                        Total = isValidTotal ? total : default(decimal)
+                    });
+                }
+            }
+
+            return customerData;
+        }
+    }
+}
